9e19e8297578a048fbe217d2e1ea2f05398004ef
alejandro gomes <alejandrogomes23@hotmail.com>
2024-10-03 21:47:09 -0300

    Primeira Api CRUD completo

---
+++ PrimeiraApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraApi.Models;
+
+namespace PrimeiraApi.DataContext
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
